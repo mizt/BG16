710f9719411a3d839bd5f6933ff5f89955e35ee7
mizt <mizt@mizt.org>
2025-06-23 17:09:25 +0900

    Initial commit

---
+++ BG16.mm
@@ -0,0 +1,158 @@
+#import <Foundation/Foundation.h>
+#import <simd/simd.h>
+#import <vector>
+#import <string>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        
+            
+        std::string JSON = R"({"meshes":[{"primitives":[{"attributes":{"POSITION":0,"RGB":1}}]}],"accessors":[{"bufferView":0,"componentType":5126,"count":0,"type":"VEC3"},{"bufferView":1,"componentType":5126,"count":0,"type":"VEC3"},],"bufferViews":[{"buffer":0,"byteLength":0,"byteOffset":0},{"buffer":0,"byteLength":0,"byteOffset":0},{"buffer":0,"byteLength":0,"byteOffset":0}],"buffers":[{"byteLength":0}]})";
+        
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:[[NSString stringWithUTF8String:JSON.c_str()] dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableContainers error:nil];
+        
+        if(dict) {
+            
+            std::vector<simd::float3> _v;
+            std::vector<simd::uchar3> _rgb;
+            std::vector<simd::uint3> _f;
+            
+            std::vector<unsigned short> v16;
+            std::vector<unsigned char> rgb;
+
+            NSCharacterSet *WHITESPACE = [NSCharacterSet whitespaceCharacterSet];
+            
+            NSString *data = [NSString stringWithContentsOfFile:@"./BG.obj" encoding:NSUTF8StringEncoding error:nil];
+            NSArray *lines = [data componentsSeparatedByCharactersInSet: [NSCharacterSet newlineCharacterSet]];
+                        
+            for(int k=0; k<lines.count; k++) {
+                NSArray *arr = [lines[k] componentsSeparatedByCharactersInSet:WHITESPACE];
+                if([arr count]>0) {
+                    if([arr[0] isEqualToString:@"v"]) {
+                        if([arr count]>=4) {
+                            _v.push_back(simd::float3{
+                                [arr[1] floatValue],
+                                [arr[2] floatValue],
+                                [arr[3] floatValue]
+                            });
+                            
+                            if([arr count]>=7) {
+                                _rgb.push_back(simd::uchar3{
+                                    (unsigned char)([arr[4] floatValue]*255.0),
+                                    (unsigned char)([arr[5] floatValue]*255.0),
+                                    (unsigned char)([arr[6] floatValue]*255.0),
+                                });
+                            }
+                            else {
+                                _rgb.push_back(simd::uchar3{
+                                    (unsigned char)(255),
+                                    (unsigned char)(255),
+                                    (unsigned char)(255),
+                                });
+                            }
+                        }
+                    }
+                    else if([arr[0] isEqualToString:@"f"]) {
+                        if([arr count]==4) {
+                            
+                            NSArray *a = [arr[1] componentsSeparatedByString:@"/"];
+                            NSArray *b = [arr[2] componentsSeparatedByString:@"/"];
+                            NSArray *c = [arr[3] componentsSeparatedByString:@"/"];
+                            
+                            _f.push_back(simd::uint3{
+                                (unsigned int)[a[0] intValue]-1,
+                                (unsigned int)[b[0] intValue]-1,
+                                (unsigned int)[c[0] intValue]-1
+                            });
+                            
+                        }
+                    }
+                }
+            }
+            
+            for(int n=0; n<_f.size(); n++) {
+                
+                _Float16 v[9] = {
+                    (_Float16)_v[_f[n].x].x,(_Float16)_v[_f[n].x].y,(_Float16)_v[_f[n].x].z,
+                    (_Float16)_v[_f[n].y].x,(_Float16)_v[_f[n].y].y,(_Float16)_v[_f[n].y].z,
+                    (_Float16)_v[_f[n].z].x,(_Float16)_v[_f[n].z].y,(_Float16)_v[_f[n].z].z,
+                };
+                
+                v16.push_back(*((unsigned short *)(v+0)));
+                v16.push_back(*((unsigned short *)(v+1)));
+                v16.push_back(*((unsigned short *)(v+2)));
+                
+                v16.push_back(*((unsigned short *)(v+3)));
+                v16.push_back(*((unsigned short *)(v+4)));
+                v16.push_back(*((unsigned short *)(v+5)));
+                
+                v16.push_back(*((unsigned short *)(v+6)));
+                v16.push_back(*((unsigned short *)(v+7)));
+                v16.push_back(*((unsigned short *)(v+8)));
+                
+                rgb.push_back(_rgb[_f[n].x].x);
+                rgb.push_back(_rgb[_f[n].x].y);
+                rgb.push_back(_rgb[_f[n].x].z);
+                
+                rgb.push_back(_rgb[_f[n].y].x);
+                rgb.push_back(_rgb[_f[n].y].y);
+                rgb.push_back(_rgb[_f[n].y].z);
+                
+                rgb.push_back(_rgb[_f[n].z].x);
+                rgb.push_back(_rgb[_f[n].z].y);
+                rgb.push_back(_rgb[_f[n].z].z);
+            }
+            
+            unsigned int offset = 0;
+            
+            dict[@"bufferViews"][0][@"byteOffset"] = [NSNumber numberWithInt:offset];
+            dict[@"bufferViews"][0][@"byteLength"] = [NSNumber numberWithInt:(v16.size())*sizeof(unsigned short)];
+            
+            dict[@"accessors"][0][@"count"] = [NSNumber numberWithInt:v16.size()/3];
+            NSLog(@"%d",[dict[@"accessors"][0][@"count"] intValue]);
+            
+            while(v16.size()%4!=0) {
+                v16.push_back(0);
+            }
+            offset+=v16.size()*sizeof(unsigned short);
+            
+            dict[@"bufferViews"][1][@"byteOffset"] = [NSNumber numberWithInt:offset];
+            dict[@"bufferViews"][1][@"byteLength"] = [NSNumber numberWithInt:rgb.size()*sizeof(unsigned char)];
+            dict[@"accessors"][1][@"count"] = [NSNumber numberWithInt:rgb.size()/3];
+            NSLog(@"%d",[dict[@"accessors"][1][@"count"] intValue]);
+            
+            while(rgb.size()%4!=0) {
+                rgb.push_back(0);
+            }
+            offset+=rgb.size()*sizeof(unsigned char);
+            
+            dict[@"buffers"][0][@"byteLength"] = [NSNumber numberWithInt:offset];
+            
+            NSMutableData *json = [[NSMutableData alloc] init];
+            [json appendData:[NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingWithoutEscapingSlashes|NSJSONWritingSortedKeys error:nil]];
+            
+            while(json.length%4!=0) {
+                [json appendBytes:new const char[1]{0x20} length:1];
+            }
+            
+            NSMutableData *glb = [[NSMutableData alloc] init];
+            [glb appendBytes:new const char[4]{'g','l','T','F'} length:4];
+            [glb appendBytes:new unsigned int[1]{2} length:4];
+            [glb appendBytes:new unsigned int[1]{((4*7)+(unsigned int)json.length)+offset} length:4];
+            [glb appendBytes:new unsigned int[1]{(unsigned int)json.length} length:4];
+            [glb appendBytes:new const char[4]{'J','S','O','N'} length:4];
+            [glb appendBytes:json.bytes length:json.length];
+            [glb appendBytes:new unsigned int[1]{offset} length:4];
+            [glb appendBytes:new const char[4]{'B','I','N',0} length:4];
+            [glb appendBytes:v16.data() length:v16.size()*sizeof(unsigned short)];
+            [glb appendBytes:rgb.data() length:rgb.size()*sizeof(unsigned char)];
+            
+            [glb writeToFile:@"./docs/BG.bin" atomically:YES];
+            
+            glb = nil;
+            json = nil;
+            dict = nil;
+        }
+    }
+}
---
+++ docs/BG16Loader.js
@@ -0,0 +1,109 @@
+const U8toU32 = (arr,offset)=>{ return arr[offset+3]<<24|arr[offset+2]<<16|arr[offset+1]<<8|arr[offset]; };
+const U32 = new Uint32Array(1);
+const U16toF32 = (u16)=>{
+	U32[0] = (u16&0x8000)<<16|((((u16>>10)&0x1F)-15+127)&0xFF)<<23|(u16&0x3FF)<<13;
+	return (new Float32Array(U32.buffer))[0];
+}
+
+export const BGLoader = Object.freeze({
+	
+	parse:(url,data,init)=>{
+		
+		const U8 = new Uint8Array(data);
+		
+		if(new TextDecoder().decode(U8.slice(0,4))==="glTF") {
+			
+			const size = U8toU32(U8,4*3);
+			if(new TextDecoder().decode(U8.slice(4*4,4*4+4))==="JSON") {
+				const json = JSON.parse(new TextDecoder().decode(U8.slice(4*5,4*5+size)));
+				if(new TextDecoder().decode(U8.slice(4*5+size+4*1,4*5+size+4*1+3))==="BIN") {
+					
+					const offset = 4*5+size+4*2;
+					const bufferViews = json["bufferViews"];
+					const byteOffsets = [bufferViews[0]["byteOffset"],bufferViews[1]["byteOffset"],bufferViews[2]["byteOffset"]];
+					const byteLengths = [bufferViews[0]["byteLength"],bufferViews[1]["byteLength"],bufferViews[2]["byteLength"]];
+
+					
+					if(+(json["accessors"][0]["count"])===+(json["accessors"][1]["count"])) {
+						
+						const U16 = (new Uint16Array(data)).slice(offset>>1);
+
+						const v16  = U16.slice(byteOffsets[0]>>1,(byteOffsets[0]+byteLengths[0])>>1);
+						const rgb8 = U8.slice(offset+byteOffsets[1],(offset+byteOffsets[1]+byteLengths[1]));
+						
+						const len = +(json["accessors"][0]["count"]);
+						
+						const v = new Float32Array(len*3);
+						for(var n=0; n<v.length; n++) {
+							v[n] = U16toF32(v16[n]);
+						}
+						
+						const rgb = new Float32Array(len*3);
+						for(var n=0; n<rgb.length; n++) {
+							rgb[n] = rgb8[n]/255.0;
+						}
+						
+						const f = new Uint32Array(len);
+						for(var n=0; n<f.length; n++) {
+							f[n] = n;
+						}
+						
+						if(v.length&&rgb.length) {
+							const result = {};
+							result[url] = {
+								"v":v,
+								"rgb":rgb,
+								"f":f,
+								"bytes":4
+							}
+							init(result);
+						}
+					}
+				}
+			}
+		}
+	},
+	
+	load:(url,init)=>{
+		
+		let list = [];
+		
+		if(typeof(url)==="string") {
+			list.push(url);
+		}
+		else if(Array.isArray(url)) {
+			list = url;
+		}
+						
+		if(list.length>=1) {
+			
+			let loaded = 0;
+			let data = {};
+			
+			const onload = (result) => {
+				const key = Object.keys(result)[0];
+				data[key] = result[key];
+				loaded++;
+				if(loaded===list.length) {
+					init(data);
+				}
+			};
+			
+			const load = (url) => {
+				fetch(url).then(response=>response.blob()).then(data=>{
+					const fr = new FileReader();
+					fr.onloadend = ()=>{
+						BGLoader.parse(url,fr.result,onload);
+					};
+					fr.readAsArrayBuffer(data)
+				}).catch(error=>{
+					console.error(error);
+				});
+			}
+			
+			for(var n=0; n<list.length; n++) {
+				load(list[n]);
+			}
+		}
+	}
+});
